7f210b9f9f900c8cb367b3222ac492738d10a9e9
Qi Chen <qichen836@gmail.com>
2014-04-08 16:50:41 -0400

    test

--- python/4_1_MaxSubArray_q.py
+++ python/4_1_MaxSubArray_q.py
@@ -1,4 +1,27 @@
-# Exercise 4.1-1
+# Chapter 4.1, Find Max SubArray
+#test
 
-define FindMaxSubArray(a):
+import sys
 
+# Original code
+# Parameter: array
+# Return: the largest sum of a subarray
+def FindMaxSubArray(oneArray):
+    start = 0
+    end   = 0
+    sum   = 0
+    result = -sys.maxint - 1
+    for i in oneArray:
+        sum += i
+        if sum < 0:
+            end += 1
+            start = end
+            sum = 0
+        elif sum > result:
+            result = sum
+            end += 1
+    print 'the larges sum is:', result, 'start:', start, 'end:', end
+#    return result
+
+a = [11,22,-21,33,44,55, -1, 10]
+FindMaxSubArray(a)
