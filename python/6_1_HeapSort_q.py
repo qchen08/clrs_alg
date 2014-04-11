c7baba03de872c005870d5d4d084575d24dfe4e0
Qi Chen <qichen836@gmail.com>
2014-04-11 14:14:54 -0400

    Change name, use only one file for chapter 6

--- python/6_1_HeapSort_q.py
+++
@@ -1,7 +0,0 @@
-# Chapter 6 Heap Sort, 4/11/2014
-
-# 6.1 Heaps
-
-
-
-
---
+++ python/6_HeapSort_q.py
@@ -0,0 +1,20 @@
+# Chapter 6 Heap Sort, 4/11/2014
+
+# 6.1 Heaps
+
+
+# Exercise 6.1-4
+# The smallest element must be a leef.
+
+# Exercise 6.1-5
+# If the array is in ascending order from the left to right, then it is a min-heap.
+
+# Exercise 6.1-6
+# No. Node 6 has a child 7 which is larger than it.
+
+# Exercise 6.1-7
+# ??
+
+
+
+
