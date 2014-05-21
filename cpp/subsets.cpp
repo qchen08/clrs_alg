3f0261c3db6c511a67e8f11dde818cb8c67feb45
Hou-jen@MacBook <ko.houjen@gmail.com>
2014-05-21 02:07:33 -0400

    subsets

--- cpp/subsets.cpp
+++ cpp/subsets.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <map>
 #include <vector>
+using namespace std;
 
+vector<vector<int> > ret;
 class BITSET{
 	private:
 		std::vector<unsigned long long> bitset;
+		int s;
 	public:
 		BITSET(int size){
 			int i;
 			for(i = 0; i < size / sizeof(unsigned long long)+1; i++){
 				bitset.push_back(0);
 			}
+			s = size;
 		}
 
 		bool operator[](int i) const{
@@ -20,6 +24,18 @@ class BITSET{
 			return (num >> reminder) & 0x1;
 		}
 
+		void print(){
+			std::cout << bitset[0] << std::endl;
+		}
+
+		bool empty() const{
+			int i;
+			for(i = 0; i < s; i++){
+				if((*this)[i])
+					return false;
+			}
+			return true;
+		}
 
 		BITSET& inc() {
 			unsigned long long  max_num = -1;
@@ -69,49 +85,84 @@ void print_set(const BITSET &bitset, const std::vector<int>& keyVec, const std::
 
 
 	while(1){
-		std::cout << "{";
+		bool output = false;
+
+		vector<int> retVec;
 		for(j = 0; j < cntVec.size(); j++){
 			if(bitset[j]){
-				std::cout << keyVec[j] << " ";
-				for(i = 0; i < cntTmp[j]; i++)
-					std::cout << keyVec[j] << " ";
+				output = true;
+				retVec.push_back(keyVec[j]);
+				for(i = 0; i < cntTmp[j]; i++){
+					retVec.push_back(keyVec[j]);
+				}
 			}
 		}
-		std::cout << "\b}" << std::endl;
+
+		ret.push_back(retVec);
 		if(!dec(bitset, cntVec, cntTmp))
 			break;
 	}
 }
 
-int main(){
-	int array[] = {1, 3, 2, 2, 3};
-	int i, j;
-	std::map<int, int> cntMap;
-	std::vector<int> keyVec;
-	std::vector<int> cntVec;
-	std::map<int, int>::iterator iter;
-	for(i = 0; i < sizeof(array)/sizeof(int); i++){
-		if((iter = cntMap.find(array[i])) != cntMap.end()){
-			iter->second ++;
-		}else{
-			cntMap.insert(std::make_pair(array[i], 1));
+class Solution {
+	public:
+		vector<vector<int> > subsetsWithDup(vector<int> &S) {
+			int i, j;
+			std::map<int, int> cntMap;
+			std::vector<int> keyVec;
+			std::vector<int> cntVec;
+			std::map<int, int>::iterator iter;
+			for(i = 0; i < S.size(); i++){
+				if((iter = cntMap.find(S[i])) != cntMap.end()){
+					iter->second ++;
+				}else{
+					cntMap.insert(std::make_pair(S[i], 1));
+				}
+			}
+
+			// create cntVec & keyVec
+			for(auto &cnt : cntMap){
+				keyVec.push_back(cnt.first);
+				cntVec.push_back(cnt.second);
+			}
+
+			int N = cntMap.size();
+			BITSET mybitset(N);
+
+			for(i = 0; i < N*N+1; i ++){
+				mybitset.inc();
+				//		mybitset.print();
+				print_set(mybitset, keyVec, cntVec);
+				if(mybitset.empty()){
+					break;
+				}
+			}
+			return ret;	
 		}
-	}
+};
 
-	// create cntVec & keyVec
-	for(auto &cnt : cntMap){
-		keyVec.push_back(cnt.first);
-		cntVec.push_back(cnt.second);
-	}
 
-	int N = cntMap.size();
-	BITSET mybitset(N);
+int main(){
+	int myints[] = {1, 1};
+	int i, j;
+	std::vector<int>  array(myints, myints + sizeof(myints) / sizeof(int) );
+	Solution s;
+	vector<vector<int> > result = s.subsetsWithDup(array);
 
-	std::cout << "{";
-	for(i = 0; i < N * N - 1; i ++){
-		print_set(mybitset, keyVec, cntVec);
-		mybitset.inc();
+	cout << "[\n";
+	for(i = 0; i < result.size(); i ++){
+		cout << "  [";
+		for(j = 0; j < result[i].size(); j++){
+			cout << result[i][j];
+			if(j != result[i].size()-1)
+				cout << ",";
+		}
+		cout << "]";
+		if(i != result.size()-1)
+			cout << ",";
+		cout << "\n";
 	}
+	cout << "]\n";
 
 	return 0;
 }
